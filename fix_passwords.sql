4815101457b78eb001f5ee556b58e343044d18a3
shehal-exe <shehaledu@gmail.com>
2026-09-24 00:46:23 +0530

    style: apply new Antigravity Healthcare Brand Spec colors, spacing, and typography

---
+++ fix_passwords.sql
@@ -0,0 +1,1 @@
+UPDATE User SET password_hash = '$2a$10$XoO5W2jG2M12b1I0PzL7xO7fA00bZ4l2sZf.U4yA.K.t1.oB12m02';
---
+++ update.sql
@@ -0,0 +1,1 @@
+UPDATE User SET password_hash = '$2a$10$8IKbQOgbc18YpZXbNLCV2uuTAewFbnFHQho2DZrWUn4ZF225pmTiK';
